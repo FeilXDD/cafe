f5a8da069df25ee709d996f3f253c177f6bf1b49
FeilXDD <165951297+FeilXDD@users.noreply.github.com>
2025-01-26 13:44:45 -0500

    inicio projecto
    se crea projecto con carpeta fron y back y se crea un json de menu con un servidor para el backend con express

---
+++ BACKEND/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express();
+
+app.disable('x-powered-by');//desabilita la cabecera de express
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send({message:'Hello World!'});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
